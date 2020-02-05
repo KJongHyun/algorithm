840cd490b8cec3699ad31d214faa5758b83278f5
jonghyeonkim <whdgus8219@naver.com>
2020-02-05 22:02:23 +0900

    20200205 - quicksort 구현

--- quicksort.cpp
+++ quicksort.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 using namespace std;
 
-int number = 10;
-int data[10] = {10, 3, 8, 5, 2, 6, 4, 1, 9, 7 };
+int number = 15;
+int data[15] = {10, 3, 8, 5, 2, 6, 4, 1, 9, 7,  31, 15, 23, 17, 29};
 
 void quickSort(int * data, int start, int end) {
     if (start >= end)
@@ -15,10 +15,12 @@ void quickSort(int * data, int start, int end) {
     int i = start + 1;
     int j = end;
     int temp;
-    while (i <= j) { // 엇갈릴 때까지 반복
+    int count = 0;
+    // 가장 바깥 while
+    while (i <= j) {
+        cout << "바깥 while : " << count++ << endl;
         while (data[key] >= data[i] && i <= end) {
             i++;
-            cout << "while i : " << i << endl;
         }
         while (data[key] <= data[j] && j > start) {
             j--;
@@ -31,7 +33,6 @@ void quickSort(int * data, int start, int end) {
             temp = data[j];
             data[j] = data[i];
             data[i] = temp;
-            cout << "key :  " << key << " end : " << end << " i : " << i <<  " j : " << j << endl;
         }
 
     }
@@ -42,8 +43,8 @@ void quickSort(int * data, int start, int end) {
 }
 
 int main() {
-    quickSort(data, 0, 9);
-    for (int i = 0 ; i < 10; i++) {
+    quickSort(data, 0, number - 1);
+    for (int i = 0 ; i < number; i++) {
         cout << data[i] << " ";
     }
     return 0;
